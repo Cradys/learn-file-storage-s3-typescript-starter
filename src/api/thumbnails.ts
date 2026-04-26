4165db01742b331965553164e7cd0d105ecbdfd4
Crady <centyrion93@gmail.com>
2026-04-26 21:08:37 +0300

    file upload with form-data

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, getVideos, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -11,6 +11,7 @@ type Thumbnail = {
 };
 
 const videoThumbnails: Map<string, Thumbnail> = new Map();
+const MAX_UPLOAD_SIZE = 10 << 20 //10MB
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -48,6 +49,34 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData()
+  const parsed = formData.get("thumbnail")
 
-  return respondWithJSON(200, null);
+  if (!(parsed instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  if (MAX_UPLOAD_SIZE < parsed.size) {
+    throw new BadRequestError("File is too big. Max 10MB");
+  }
+
+  const mediaType = parsed.type
+  const imageData = await parsed.arrayBuffer()
+  
+  const videoMetadata = getVideo(cfg.db, videoId)
+  console.log(videoMetadata, userID)
+
+  if (!videoMetadata || videoMetadata.userID !== userID) {
+    throw new UserForbiddenError("")
+  }
+
+  videoThumbnails.set(videoMetadata.id, {data: imageData, mediaType: mediaType})
+
+  videoMetadata.thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`
+
+  updateVideo(cfg.db, videoMetadata)
+
+  
+
+  return respondWithJSON(200, videoMetadata);
 }
